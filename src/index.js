3b77636a07929d13a9d5b9e7476bf00d6f70f3b1
elliotaplant <elliot@anthropic.com>
2025-03-24 14:41:34 -0700

    Take two with worker

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { addMemos } from './addMemos';
+import { registerOrder } from './registerOrder';
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async scheduled(controller, env, ctx) {
+		console.log('Cron triggered');
+		await addMemos();
+	},
+
+	async fetch() {
+		// if post to add email
+		//    body should be an email content
+		//    registerOrder
+		// if post to add memos
+		//    addMemosToAmazonTransactions();
+		// else
+		//    404
+	},
+
+	async email(message, env, ctx) {
+		// Get email text content
+		const emailText = await message.text();
+		await registerOrder(emailText);
 	},
 };
